27c817cc5a4a2a0ff8cc4af1220635050afaa5d3
Pianczetti <piankapf95@gmail.com>
2023-02-16 12:19:06 +0100

    printReport finished

--- crawl.js
+++ crawl.js
@@ -1,27 +1,53 @@
 const { JSDOM } = require('jsdom')
 
-async function crawlPage(currentURL){
-    console.log(`actively crawling ${currentURL}`);
 
+async function crawlPage(baseURL, currentURL, pages){
+
+    const baseURLObj = new URL(baseURL);
+    const currentURLObj = new URL(currentURL);
+
+    if(currentURLObj.hostname !== baseURLObj.hostname){
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL);
+    if(pages[normalizedCurrentURL] > 0){
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1;
+
+    console.log(`actively crawling ${currentURL}`);
+    let htmlBody = ''
     try{
         const resp = await fetch(currentURL);
 
         if(resp.status > 399){
-            console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`);
-            return
+            console.log(`Got HTTP error, status code: ${resp.status} on page: ${currentURL}`);
+            return pages
         }
 
         const contentType = resp.headers.get('content-type');
 
         if(!contentType.includes('text/html')){
             console.log(`non html response, content type: ${contentType} on page: ${currentURL}`);
-            return
+            return pages
         }
 
-        console.log(await resp.text());
+        htmlBody = await resp.text()
+
     } catch (err){
         console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
     }
+
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+
+    for(const nextURL of nextURLs){
+        pages = await crawlPage(baseURL, nextURL, pages)
+    }
+    
+    return pages
 
 }
 
@@ -44,7 +70,7 @@ function getURLsFromHTML(htmlBody, baseURL){
                 const urlObj = new URL(linkElement.href)
                 urls.push(urlObj.href)
             } catch (err) {
-                console.log(`error with absolute url: ${err.message}`)
+                console.log(`error with absolute url: ${err.message}: ${linkElement.href}`)
             }
             
         }
